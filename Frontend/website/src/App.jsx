28648b5c19a11b76523facfd17a94bd3bc5b5016
raghava17119 <219x1a05f4@gprec.ac.in>
2025-03-08 23:32:56 +0530

    axios

--- Frontend/website/src/App.jsx
+++ Frontend/website/src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
@@ -29,15 +30,20 @@ function App() {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch('http://localhost:3000/predict', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ parameters: Object.values(parameters) }),
-    });
-    const data = await response.json();
-    alert(`Prediction: ${data.prediction}`);
+
+    try {
+      const response = await axios.post(
+        'https://backend-solar-irradiance-prediction.onrender.com/predict',
+        {
+          parameters: Object.values(parameters).map((val) => parseFloat(val) || 0),
+        }
+      );
+
+      alert(`Prediction: ${response.data.prediction}`);
+    } catch (error) {
+      console.error('Error making prediction:', error);
+      alert('Error predicting solar irradiance. Please try again.');
+    }
   };
 
   return (
@@ -46,136 +52,69 @@ function App() {
       <form onSubmit={handleSubmit}>
         <div>
           <label>Temperature:</label>
-          <input
-            type="text"
-            name="temperature"
-            value={parameters.temperature}
-            onChange={handleChange}
-          />
+          <input type="text" name="temperature" value={parameters.temperature} onChange={handleChange} />
         </div>
         <div>
           <label>Pressure:</label>
-          <input
-            type="text"
-            name="pressure"
-            value={parameters.pressure}
-            onChange={handleChange}
-          />
+          <input type="text" name="pressure" value={parameters.pressure} onChange={handleChange} />
         </div>
         <div>
           <label>Humidity:</label>
-          <input
-            type="text"
-            name="humidity"
-            value={parameters.humidity}
-            onChange={handleChange}
-          />
+          <input type="text" name="humidity" value={parameters.humidity} onChange={handleChange} />
         </div>
         <div>
           <label>Wind Direction (Degrees):</label>
-          <input
-            type="text"
-            name="windDirection"
-            value={parameters.windDirection}
-            onChange={handleChange}
-          />
+          <input type="text" name="windDirection" value={parameters.windDirection} onChange={handleChange} />
         </div>
         <div>
           <label>Speed:</label>
-          <input
-            type="text"
-            name="speed"
-            value={parameters.speed}
-            onChange={handleChange}
-          />
+          <input type="text" name="speed" value={parameters.speed} onChange={handleChange} />
         </div>
         <div>
           <label>Month:</label>
-          <input
-            type="text"
-            name="month"
-            value={parameters.month}
-            onChange={handleChange}
-          />
+          <input type="text" name="month" value={parameters.month} onChange={handleChange} />
         </div>
         <div>
           <label>Day:</label>
-          <input
-            type="text"
-            name="day"
-            value={parameters.day}
-            onChange={handleChange}
-          />
+          <input type="text" name="day" value={parameters.day} onChange={handleChange} />
         </div>
-          <h2> time :</h2>
-        <div id='time'>
-        <div>
-          <label>Hour:</label>
-          <input
-            type="text"
-            name="hour"
-            value={parameters.hour}
-            onChange={handleChange}
-            />
+
+        <h2>Time:</h2>
+        <div id="time">
+          <div>
+            <label>Hour:</label>
+            <input type="text" name="hour" value={parameters.hour} onChange={handleChange} />
+          </div>
+          <div>
+            <label>Minute:</label>
+            <input type="text" name="minute" value={parameters.minute} onChange={handleChange} />
+          </div>
+          <div>
+            <label>Second:</label>
+            <input type="text" name="second" value={parameters.second} onChange={handleChange} />
+          </div>
         </div>
-        <div>
-          <label>Minute:</label>
-          <input
-            type="text"
-            name="minute"
-            value={parameters.minute}
-            onChange={handleChange}
-            />
+
+        <h2>Sunrise and Sunset Time:</h2>
+        <div id="time">
+          <div>
+            <label>Rise Hour:</label>
+            <input type="text" name="risehour" value={parameters.risehour} onChange={handleChange} />
+          </div>
+          <div>
+            <label>Rise Minute:</label>
+            <input type="text" name="riseminuter" value={parameters.riseminuter} onChange={handleChange} />
+          </div>
+          <div>
+            <label>Set Hour:</label>
+            <input type="text" name="sethour" value={parameters.sethour} onChange={handleChange} />
+          </div>
+          <div>
+            <label>Set Minute:</label>
+            <input type="text" name="setminute" value={parameters.setminute} onChange={handleChange} />
+          </div>
         </div>
-        <div>
-          <label>Second:</label>
-          <input
-            type="text"
-            name="second"
-            value={parameters.second}
-            onChange={handleChange}
-            />
-      </div>
-        </div >
-          <h2>Sunrise and Sunset Time:</h2>
-        <div id='time'>
-        <div >
-          <label>Rise Hour:</label>
-          <input
-            type="text"
-            name="risehour"
-            value={parameters.risehour}
-            onChange={handleChange}
-            />
-        </div>
-        <div>
-          <label>Rise Minute:</label>
-          <input
-            type="text"
-            name="riseminuter"
-            value={parameters.riseminuter}
-            onChange={handleChange}
-            />
-        </div>
-        <div>
-          <label>Set Hour:</label>
-          <input
-            type="text"
-            name="sethour"
-            value={parameters.sethour}
-            onChange={handleChange}
-            />
-        </div>
-        <div>
-          <label>Set Minute:</label>
-          <input
-            type="text"
-            name="setminute"
-            value={parameters.setminute}
-            onChange={handleChange}
-            />
-        </div>
-            </div>
+
         <button type="submit">Predict</button>
       </form>
     </div>
